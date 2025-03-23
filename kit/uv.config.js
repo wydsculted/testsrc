8a2661c9a9a105bc841f86d639c4fd06c435cb40
wydsculted <aidanjoshua934@gmail.com>
2025-03-23 19:27:20 -0400

    Add files via upload

---
+++ kit/uv.config.js
@@ -0,0 +1,11 @@
+self.__uv$config = {
+    prefix: '/securly/who/',
+    bare:'https://goto.securlywho.unblocked.gg/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/securly/who/uv.handler.js',
+    bundle: '/securly/who/uv.bundle.js',
+    bundle: '/securly/who/uv.bundle.js',
+    config: '/securly/who/uv.config.js',
+    sw: '/securly/who/uv.sw.js',
+};
